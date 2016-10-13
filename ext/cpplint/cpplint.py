eaf240ed93a209e5444439effeef775916c04197
Liam Tyler <tyler147@umn.edu>
2016-10-13 14:23:19 -0500

    hook ready

--- ext/cpplint/cpplint.py
+++ ext/cpplint/cpplint.py
@@ -6114,7 +6114,7 @@ def main():
     ProcessFile(filename, _cpplint_state.verbose_level)
   _cpplint_state.PrintErrorCounts()
 
-  sys.exit(_cpplint_state.error_count > 0)
+  sys.exit(_cpplint_state.error_count)
 
 
 if __name__ == '__main__':
