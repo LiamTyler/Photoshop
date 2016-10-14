9b5998db68926d9bdc1c7b2a8fb12c89cc5db3fc
shashank <yalla003@umn.edu>
2016-10-14 12:14:12 -0500

    Fixed tool.h coding style

--- include/tool.h
+++ include/tool.h
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Name            : tool.h
+ * Project         : BrushWork
+ * Module          : App
+ * Description     : Header of tool
+ * Copyright       : 2016 CSCI3081W Team_Bits_Please. All rights reserved.
+ * Creation Date   : 2/15/15
+ * Original Author : Seth Johnson
+ *
+ ******************************************************************************/
+
 #ifndef INCLUDE_TOOL_H_
 #define INCLUDE_TOOL_H_
 
@@ -5,18 +16,19 @@
 #include "include/pixel_buffer.h"
 
 class Tool {
-    public:
-        Tool(int width, int height);
-        Tool();
-        ~Tool();
-        void applyTool(image_tools::PixelBuffer* buff, image_tools::ColorData current_color, int x, int y);
-        int getWidth();
-	int getHeight();
-	
-    private:
-        int width_;
-        int height_;
-        double** mask_;
+ public:
+  Tool(int width, int height);
+  Tool();
+  ~Tool();
+  void applyTool(image_tools::PixelBuffer* buff,
+  image_tools::ColorData current_color, int x, int y);
+  int getWidth();
+  int getHeight();
+
+ private:
+  int width_;
+  int height_;
+  double** mask_;
 };
 
-#endif
+#endif  // INCLUDE_TOOL_H_
