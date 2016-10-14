947acc9364653c46434ac605b6e84d8f14d54ceb
Liam Tyler <tyler147@umn.edu>
2016-10-14 11:33:50 -0500

    updating

--- include/tool_utilities.h
+++ include/tool_utilities.h
@@ -1,3 +1,20 @@
+#ifndef INCLUDE_TOOL_UTILITIES_H_
+#define INCLUDE_TOOL_UTILITIES_H_
+
+
+/*******************************************************************************
+ * Name            : tool_utilities.h
+ * Project         : BrushWork
+ * Module          : utils
+ * Description     : Header file for tool_utilities namespace + functions
+ * Copyright       : 2016 CSCI3081 Group Bits Please
+ * Creation Date   : 10/14/16
+ * Original Author : Liam Tyler
+ *
+ ******************************************************************************/
+
 namespace tool_utilities {
     void createCircle(double** mask, int height, int width, double radius);
 }
+
+#endif  // INCLUDE_TOOL_UTILITIES_H_
