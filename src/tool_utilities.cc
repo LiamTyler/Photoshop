26827a813e657035e299a02dfb113bb7fca744af
Liam Tyler <tyler147@umn.edu>
2016-10-14 11:32:51 -0500

    adding

--- src/tool_utilities.cc
+++ src/tool_utilities.cc
@@ -3,16 +3,16 @@
 
 namespace tool_utilities {
 
-    void createCircle(double** mask, int height, int width, double radius) {
+    void createCircle (double** mask, int height, int width, double radius) {
         int cent_x = width / 2;
         int cent_y = height / 2;
-        for(int h = 0; h < height; h++) {
-            for(int w = 0; w < width; w++) {
-                if(pow(w - cent_x, 2) + pow(h - cent_y, 2) <= radius * radius) {
+        for (int h = 0; h < height; h++) {
+            for (int w = 0; w < width; w++) {
+                if (pow(w - cent_x, 2) + pow(h - cent_y, 2) <= radius * radius) {
                     mask[h][w] = 1.0; 
                 }
             }
         }
     }
 
-}
+}  // namespace tool_utilities
