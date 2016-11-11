0a1c5531dd6dc7f459a62493f3dd4d1e230502ad
Liam Tyler <tyler147@umn.edu>
2016-11-11 12:55:08 -0600

    fix(blur_filter): turns out there was a bug. Near the edges where the kernal does not get fully applied, the new pixel didn't add up to 1, so the edges turned to black. Fixed by adding in a percentage of the bg color, based on how much of the kernal was not applied. Contributors: tyler147, hende702

--- src/filter.cc
+++ src/filter.cc
@@ -38,24 +38,29 @@ void Filter::CreateKernal(int width, int height) {
     AllocateKernal(width, height);
     std::cout << "Kernal: w = " << width_ << ", h = " << height_ << std::endl;
     InitializeKernal();
+    /* Print out kernal to check if the initialize worked
     for (int r = 0; r < height_; r++) {
         for (int c = 0; c < width_; c++) {
             std::cout << kernal_[r][c] << " ";
         }
         std::cout << std::endl;
     }
+    */
 }
 
 void Filter::ApplyFilter(PixelBuffer* oldimage, PixelBuffer* newimage) {
     int buff_height = oldimage->height();
     int buff_width = oldimage->width();
     ColorData total;
+    ColorData bgc = oldimage->background_color();
     int kern_mid_x = width_ / 2;
     int kern_mid_y = height_ / 2;
+    double applied;
 
     for (int r = 0; r < buff_height; r++) {
         for (int c = 0; c < buff_width; c++) {
             total = ColorData(0, 0, 0);
+            applied = 0;
             // Center the kernal over the pixel, and apply it by
             // getting the running total of pixel * intensity
             for (int kr = 0; kr < height_; kr++) {
@@ -63,12 +68,17 @@ void Filter::ApplyFilter(PixelBuffer* oldimage, PixelBuffer* newimage) {
                     int cur_x = c - kern_mid_x + kc;
                     int cur_y = r - kern_mid_y + kr;
                     if (0 <= cur_x && cur_x < buff_width &&
-                        0 <= cur_y && cur_y < buff_height)
+                        0 <= cur_y && cur_y < buff_height) {
+                            applied += 1;
                         total = total + oldimage->get_pixel(cur_x, cur_y)
                                         * kernal_[kr][kc];
+                    }
                 }
             }
-            newimage->set_pixel(c, r, total);
+            // Adjust color if the mask was partially off the edge
+            double p = applied / (width_ * height_);
+            ColorData adjusted = total + (bgc * (1 / p));
+            newimage->set_pixel(c, r, adjusted);
             // std::cout << "Pixel at (" << r << "," << c << "): "
             // << total << std::endl;
         }
