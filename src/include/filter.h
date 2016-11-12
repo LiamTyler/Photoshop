9472bbafdd467f50a9a7526d7706cdba66538381
Liam Tyler <tyler147@umn.edu>
2016-11-12 11:59:30 -0600

    refactor(project): Refactored the kernals into a base FILTER class, where KERNAL_FILTER and SIMPLE_FILTER both derive from that, and both of those are abstract. Contributors: tyler147

--- src/include/filter.h
+++ src/include/filter.h
@@ -19,27 +19,13 @@ using image_tools::PixelBuffer;
 
 class Filter {
  public:
-  Filter();
-  Filter(int width, int height);
-  virtual ~Filter();
-  virtual void ApplyFilter(PixelBuffer* oldimage, PixelBuffer* newimage);
-  virtual void CreateKernal(int width, int height);
-  inline int get_width() { return width_; }
-  inline int get_height() { return height_; }
-  inline double** get_kernal() { return kernal_; }
+  virtual ~Filter() {}
+  virtual void ApplyFilter(PixelBuffer* oldimage, PixelBuffer* newimage) = 0;
   virtual std::string name(void) = 0;
 
- protected:
-  void AllocateKernal(int width, int height);
-  void DeallocateKernal();
-  virtual void InitializeKernal() = 0;
-
- private:
-  Filter(const Filter &f) = delete;
-  Filter& operator=(const Filter &f) = delete;
-  int width_;
-  int height_;
-  double** kernal_;
+// private:
+// Filter(const Filter &f) = delete;
+// Filter& operator=(const Filter &f) = delete;
 };
 
 #endif  // SRC_INCLUDE_FILTER_H_
