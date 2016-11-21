5c94adc0dc79734faf211de2c897743a4c5378f3
yalla003 <yalla003@umn.edu>
2016-11-20 20:07:36 -0600

    Add files via upload

---
+++ src/include/image_handler.h
@@ -0,0 +1,23 @@
+#ifndef SRC_INCLUDE_IMAGEHANDLER_
+#define SRC_INCLUDE_IMAGEHANDLER_
+
+#include <string>
+#include "include/pixel_buffer.h"
+
+using image_tools::PixelBuffer;
+
+class ImageHandler {
+public:
+    enum IMAGE_TYPE {
+        PNG_IMAGE,
+        JPG_IMAGE,
+        UNKNOWN_IMAGE
+    };
+
+    static PixelBuffer* loadImage(const std::string & fileName);
+    static bool saveImage(const std::string & fileName, const PixelBuffer* bufferToSave);
+private:
+    static IMAGE_TYPE getImageType(const std::string & fileName);
+};
+
+#endif // SRC_INCLUDE_IMAGEHANDLER_
